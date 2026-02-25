1437cfa5fca6a0027a353c17e24d4be7ff5ee4c4
Betacom <Betacom@BETACOM-PC2089>
2026-02-25 12:30:58 +0100

    ultimo commit prima della nuke

--- src/main/java/com/betacom/jpa/services/interfaces/ISospensioniServices.java
+++ src/main/java/com/betacom/jpa/services/interfaces/ISospensioniServices.java
@@ -1,5 +1,20 @@
 package com.betacom.jpa.services.interfaces;
 
+
+import java.util.List;
+
+import com.betacom.jpa.dto.inputs.MarcaRequest;
+import com.betacom.jpa.dto.inputs.SospensioniRequest;
+import com.betacom.jpa.dto.outputs.MarcaDTO;
+import com.betacom.jpa.dto.outputs.SospensioniDTO;
+
 public interface ISospensioniServices {
 
+
+
+	void create(SospensioniRequest req) throws Exception;
+	void update(SospensioniRequest req) throws Exception;
+	void delete(Integer id) throws Exception;
+
+	List<SospensioniDTO> list();
 }
