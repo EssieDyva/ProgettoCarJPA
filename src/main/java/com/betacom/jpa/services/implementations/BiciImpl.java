d6dc7a0024fe7f4947264d44273fb5b2cad026f6
lucaricca20-blip <lucaricca20@gmail.com>
2026-02-25 17:26:40 +0100

    Merge branch 'develop' of https://github.com/EssieDyva/ProgettoCarJPA.git into develop/luca

--- src/main/java/com/betacom/jpa/services/implementations/BiciImpl.java
+++ src/main/java/com/betacom/jpa/services/implementations/BiciImpl.java
@@ -1,5 +1,81 @@
 package com.betacom.jpa.services.implementations;
 
-public class BiciImpl {
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.betacom.jpa.dto.inputs.BiciRequest;
+import com.betacom.jpa.dto.outputs.BiciDTO;
+import com.betacom.jpa.exception.AcademyException;
+import com.betacom.jpa.models.Bici;
+import com.betacom.jpa.models.Veicoli;
+import com.betacom.jpa.repository.IBiciRepository;
+import com.betacom.jpa.repository.IVeicoliRepository;
+import com.betacom.jpa.services.interfaces.IBiciServices;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class BiciImpl implements IBiciServices  {
+
+	private final IBiciRepository bicR;
+	private final IVeicoliRepository veiR;
+	
+	@Override
+	public void create(BiciRequest req) throws Exception {
+		log.debug("create []", req);
+		Veicoli v = veiR.findById(req.getId())
+                .orElseThrow(() -> new AcademyException("Veicolo non trovato"));
+		Bici bici = new Bici();
+		bici.setId(v.getId());
+		bici.setIdFreno(req.getIdFreno());
+		bici.setIdSospensioni(req.getIdSospensioni());
+		bici.setIsPieghevole(req.getIsPieghevole());
+		bici.setNumeroMarce(req.getNumeroMarce());
+		
+		bicR.save(bici);
+		
+	}
+
+	@Override
+	public void update(BiciRequest req) throws Exception {
+		log.debug("update {}", req);
+		Bici bici = bicR.findById(req.getId())
+				.orElseThrow(() -> new AcademyException("Bici non trovata"));
+		bici.setIdFreno(req.getIdFreno());
+		bici.setIdSospensioni(req.getIdSospensioni());
+		bici.setIsPieghevole(req.getIsPieghevole());
+		bici.setNumeroMarce(req.getNumeroMarce());
+		
+		bicR.save(bici);
+	}
+
+	@Override
+	public void delete(Integer id) throws Exception {
+		log.debug("delete {}", id);
+		Bici bici = bicR.findById(id)
+				.orElseThrow(() -> new AcademyException("Bici non trovata"));
+		
+		bicR.delete(bici);
+	}
+
+	@Override
+	public List<BiciDTO> list() {
+		log.debug("list {}");
+		List<Bici> lB = bicR.findAll();
+		return lB.stream()
+				.map(b -> BiciDTO.builder()
+						.id(b.getId())
+						.idFreno(b.getIdFreno())
+						.idSospensioni(b.getIdSospensioni())
+						.isPieghevole(b.getIsPieghevole())
+						.numeroMarce(b.getNumeroMarce())
+						.build())
+				.collect(Collectors.toList());	
+	}
 
 }
--- src/main/java/com/betacom/jpa/services/interfaces/IBiciServices.java
+++ src/main/java/com/betacom/jpa/services/interfaces/IBiciServices.java
@@ -1,5 +1,16 @@
 package com.betacom.jpa.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.jpa.dto.inputs.BiciRequest;
+import com.betacom.jpa.dto.outputs.BiciDTO;
+
 public interface IBiciServices {
+	
+	void create(BiciRequest req) throws Exception;
+	void update(BiciRequest req) throws Exception;
+	void delete(Integer id) throws Exception;
+	
+	List<BiciDTO> list();
 
 }
