be99165e3785ea8b0f85fb126570706d808b12e3
Betacom <Betacom@BETACOM-PC2089>
2026-02-25 10:55:20 +0100

    commit freni/marca senza imp

--- src/main/java/com/betacom/jpa/dto/inputs/MarcaRequest.java
+++ src/main/java/com/betacom/jpa/dto/inputs/MarcaRequest.java
@@ -1,5 +1,12 @@
 package com.betacom.jpa.dto.inputs;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class MarcaRequest {
 
+	private Integer id;
+	private String description;
 }
--- src/main/java/com/betacom/jpa/services/interfaces/IMarcaServices.java
+++ src/main/java/com/betacom/jpa/services/interfaces/IMarcaServices.java
@@ -1,5 +1,16 @@
 package com.betacom.jpa.services.interfaces;
 
+
+import java.util.List;
+
+import com.betacom.jpa.dto.inputs.MarcaRequest;
+import com.betacom.jpa.dto.outputs.MarcaDTO;
+
 public interface IMarcaServices {
 
+	List<MarcaDTO> listMarche();
+
+	void create(MarcaRequest req) throws Exception;
+	void update(MarcaRequest req) throws Exception;
+	void delete(Integer id) throws Exception;
 }
