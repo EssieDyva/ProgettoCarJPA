41aa0703bd54c8d9e8a40414ccba2ccd5479469d
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2026-02-25 10:44:31 +0100

    tipo alimentazione implemented

--- src/main/java/com/betacom/jpa/models/TipoAlimentazione.java
+++ src/main/java/com/betacom/jpa/models/TipoAlimentazione.java
@@ -1,5 +1,22 @@
 package com.betacom.jpa.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table (name = "tipo_alimentazione")
+@Data
 public class TipoAlimentazione {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true)
+	private String description;
 }
--- src/main/java/com/betacom/jpa/services/implementations/TipoAlimentazioneImpl.java
+++ src/main/java/com/betacom/jpa/services/implementations/TipoAlimentazioneImpl.java
@@ -1,5 +1,84 @@
 package com.betacom.jpa.services.implementations;
 
-public class TipoAlimentazioneImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.betacom.jpa.dto.inputs.TipoAlimentazioneRequest;
+import com.betacom.jpa.dto.outputs.TipoAlimentazioneDTO;
+import com.betacom.jpa.exception.AcademyException;
+import com.betacom.jpa.models.TipoAlimentazione;
+import com.betacom.jpa.repository.ITipoAlimentazioneRepository;
+import com.betacom.jpa.services.interfaces.ITipoAlimentazioneServices;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class TipoAlimentazioneImpl implements ITipoAlimentazioneServices {
+	
+	private final ITipoAlimentazioneRepository taR;
+	
+	@Transactional (rollbackFor = Exception.class)
+	@Override
+	public void create(TipoAlimentazioneRequest req) throws Exception {
+		log.debug("create {}", req);
+		Boolean exist = taR.findByDescription(req.getDescription());
+		
+		if (exist) 
+			throw new AcademyException("Tipo alimentazione già presente in DB");
+		
+		TipoAlimentazione tipoA = new TipoAlimentazione();
+		tipoA.setDescription(req.getDescription());
+		
+		taR.save(tipoA);
+		
+	}
+
+	@Transactional (rollbackFor = Exception.class)
+	@Override
+	public void update(TipoAlimentazioneRequest req) throws Exception {
+		log.debug("update {}", req);
+		Optional<TipoAlimentazione> tipoA = taR.findById(req.getId());
+		if (tipoA.isEmpty())
+			throw new AcademyException("Tipo alimentazione non trovato in DB");
+		
+		Boolean exist = taR.findByDescription(req.getDescription());
+		if (exist) 
+			throw new AcademyException("Tipo alimentazione già presente in DB");
+		
+		TipoAlimentazione t = tipoA.get();
+		if(req.getDescription() != null)
+			t.setDescription(req.getDescription());
+		
+		taR.save(t);
+	}
+
+	@Transactional (rollbackFor = Exception.class)
+	@Override
+	public void delete(Integer id) throws Exception {
+		log.debug("delete {}", id);
+		Optional<TipoAlimentazione> tipoA = taR.findById(id);
+		if (tipoA.isEmpty())
+			throw new AcademyException("Tipo alimentazione non trovato in DB");
+		
+		taR.delete(tipoA.get());
+	}
+
+	@Override
+	public List<TipoAlimentazioneDTO> list() throws Exception {
+		log.debug("list");
+		List<TipoAlimentazione> lT = taR.findAll();
+		return lT.stream()
+				.map(t -> TipoAlimentazioneDTO.builder()
+						.id(t.getId())
+						.description(t.getDescription())
+						.build()
+						).toList();
+	}
 
 }
