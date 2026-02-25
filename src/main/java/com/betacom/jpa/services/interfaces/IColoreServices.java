6e553ca8319a9fb1e42a32af1eb3aee761eae382
lucaricca20-blip <lucaricca20@gmail.com>
2026-02-25 11:33:01 +0100

    first Commit Luca

--- src/main/java/com/betacom/jpa/services/interfaces/IColoreServices.java
+++ src/main/java/com/betacom/jpa/services/interfaces/IColoreServices.java
@@ -1,5 +1,15 @@
 package com.betacom.jpa.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.jpa.dto.inputs.ColoreRequest;
+import com.betacom.jpa.dto.outputs.ColoreDTO;
+
 public interface IColoreServices {
+	void create(ColoreRequest req) throws Exception;
+	void update(ColoreRequest req) throws Exception;
+	void delete(Integer id) throws Exception;
+	
+	List<ColoreDTO> list();
 
 }
