13f5efbc6525a0a400cb06c6eb0d69714995fc8f
Kristian979 <kristian_preci99@live.com>
2026-02-26 17:50:39 +0100

    Builder per Veicoli in classi

--- src/main/java/com/betacom/jpa/services/implementations/VeicoliImpl.java
+++ src/main/java/com/betacom/jpa/services/implementations/VeicoliImpl.java
@@ -8,14 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.betacom.jpa.dto.inputs.VeicoliRequest;
+import com.betacom.jpa.dto.outputs.MacchinaDTO;
 import com.betacom.jpa.dto.outputs.VeicoliDTO;
 import com.betacom.jpa.exception.AcademyException;
 import com.betacom.jpa.models.*;
-import com.betacom.jpa.repository.ICategoriaRepository;
-import com.betacom.jpa.repository.IColoreRepository;
-import com.betacom.jpa.repository.IMarcaRepository;
-import com.betacom.jpa.repository.ITipoAlimentazioneRepository;
-import com.betacom.jpa.repository.IVeicoliRepository;
+import com.betacom.jpa.repository.*;
 import com.betacom.jpa.services.interfaces.IVeicoliServices;
 
 import lombok.RequiredArgsConstructor;
@@ -26,11 +23,17 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class VeicoliImpl implements IVeicoliServices {
 
+    private final IMacchinaRepository IMacchinaRepository;
+
     private final IVeicoliRepository veiR;
     private final ICategoriaRepository catR;
     private final ITipoAlimentazioneRepository taR;
     private final IColoreRepository colR;
     private final IMarcaRepository marR;
+
+    VeicoliImpl(IMacchinaRepository IMacchinaRepository) {
+        this.IMacchinaRepository = IMacchinaRepository;
+    }
 
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -122,5 +125,94 @@ public class VeicoliImpl implements IVeicoliServices {
             throw new AcademyException("Un ID inserito non esiste nella corrispettiva tabella");
         }
     }
+
+	@Override
+	public List<VeicoliDTO> selectAll(Integer id, String tipoVeicolo, Integer numeroRuote, String tipoAlimentazione, String categoria, String colore, String marca, Integer annoProduzione, String modello
+									 ,Integer porte, String targaMac, Integer cilindrata
+									 ,String targaMo, Integer cc
+									 ,Integer numeroMarce, String freno, String sospensioni, Boolean isPieghevole ) throws Exception {
+		
+		log.debug("select {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {} / {}",
+				 id, tipoVeicolo, numeroRuote, tipoAlimentazione, categoria, colore, marca, annoProduzione, modello
+				 , porte, targaMac, cilindrata
+				 , targaMo,  cc
+				 , numeroMarce,  freno,  sospensioni,  isPieghevole);	
+		
+		List<Veicoli> lV = veiR.selectAll(id, tipoVeicolo, numeroRuote, tipoAlimentazione, categoria, colore, marca, annoProduzione, modello
+				 , porte, targaMac, cilindrata
+				 , targaMo,  cc
+				 , numeroMarce,  freno,  sospensioni,  isPieghevole);
+		
+		return buildVeicoloDTO(lV);
+	}
+
+	private List<VeicoliDTO> buildVeicoloDTO(List<Veicoli> lV) {
+		return lV.stream().map(v -> VeicoliDTO.builder()
+			.id(v.getId())
+			.tipoVeicolo(v.getTipoVeicolo())
+			.numeroRuote(v.getNumeroRuote())
+			.tipoAlimentazione(v.getTipoAlimentazione())
+			.categoria(v.getCategoria())
+			.colore(v.getColore())
+			.marca(v.getMarca())
+			.annoProduzione(v.getAnnoProduzione())
+			.modello(v.getModello())
+			
+			
+			.macchina((v.getMacchina() == null) ? null : MacchinaDTO.builder()
+					.porte(v.getMacchina.getPorte())
+					.targa(v.getMacchina().getTarga())
+					.cilindrata(v.getMacchina().getCilindrata())
+					).build())
+				
+			.moto((v.getMoto() == null) ? null : MotoDTO.builder()
+					
+					
+					
+					
+					)
+			
+		}
+		
+		
+	}
+	
+//	private List<VeicoliDTO> buildVeicoloDTO(List<Veicoli> lV) {
+//	    return lV.stream().map(v -> {
+//	        // Build base (Campi comuni a tutti)
+//	        VeicoliDTO.VeicoliDTOBuilder builder = VeicoliDTO.builder()
+//	            .id(v.getId())
+//	            .tipoVeicolo(v.getTipoVeicolo() != null ? v.getTipoVeicolo() : null)
+//	            .numeroRuote(v.getNumeroRuote())
+//	            .tipoAlimentazione(v.getTipoAlimentazione() != null ? v.getTipoAlimentazione() : null)
+//	            .categoria(v.getCategoria() != null ? v.getCategoria() : null)
+//	            .colore(v.getColore() != null ? v.getColore() : null)
+//	            .marca(v.getMarca() != null ? v.getMarca() : null)
+//	            .annoProduzione(v.getAnnoProduzione())
+//	            .modello(v.getModello());
+//
+//	        // Dati specifici Macchina
+//	        if (v.getMacchina() != null) {
+//	            builder.porte(v.getMacchina().getPorte())
+//	                   .targaMac(v.getMacchina().getTarga())
+//	                   .cilindrata(v.getMacchina().getCilindrata());
+//	        }
+//	        // Dati specifici Moto
+//	        if (v.getMoto() != null) {
+//	            builder.targaMo(v.getMoto().getTarga())
+//	                   .cc(v.getMoto().getCc());
+//	        }
+//	        // Dati specifici Bici
+//	        if (v.getBici() != null) {
+//	            builder.numeroMarce(v.getBici().getNumeroMarce())
+//	                   .isPieghevole(v.getBici().getIsPieghevole())
+//	                   .freno(v.getBici().getFreni() != null ? ((Freni) v.getBici().getFreni()).getDescription() : null)
+//	                   .sospensioni(v.getBici().getSospensioni() != null ? ((Sospensioni) v.getBici().getSospensioni()).getDescription() : null);
+//	        }
+//
+//	        return builder.build();
+//	    }).toList();
+//	}
+
 
 }
