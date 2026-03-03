352228397c67418a6673c313ddcf11472b9aefb2
Kristian979 <kristian_preci99@live.com>
2026-03-03 15:06:09 +0100

    aggiunte chiamate .class e dipendenze file pom

--- src/test/java/com/betacom/jpa/ProjectCarJpaApplicationTests.java
+++ src/test/java/com/betacom/jpa/ProjectCarJpaApplicationTests.java
@@ -1,7 +1,34 @@
 package com.betacom.jpa;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.betacom.jpa.controllers.BiciControllerTest;
+import com.betacom.jpa.controllers.CategoriaControllerTest;
+import com.betacom.jpa.controllers.ColoreControllerTest;
+import com.betacom.jpa.controllers.FreniControllerTest;
+import com.betacom.jpa.controllers.MacchinaControllerTest;
+import com.betacom.jpa.controllers.MotoControllerTest;
+import com.betacom.jpa.controllers.SospensioniControllerTest;
+import com.betacom.jpa.controllers.TipoAlimentazioneControllerTest;
+import com.betacom.jpa.controllers.VeicoliControllerTest;
+
+
+
+@Suite
+@SelectClasses({
+	BiciControllerTest.class,
+	CategoriaControllerTest.class,
+	ColoreControllerTest.class,
+	FreniControllerTest.class,
+	MacchinaControllerTest.class,
+	MotoControllerTest.class,
+	SospensioniControllerTest.class,
+	TipoAlimentazioneControllerTest.class,
+	VeicoliControllerTest.class
+})
 
 @SpringBootTest
 class ProjectCarJpaApplicationTests {
@@ -9,5 +36,7 @@ class ProjectCarJpaApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	public String test;
 
 }
